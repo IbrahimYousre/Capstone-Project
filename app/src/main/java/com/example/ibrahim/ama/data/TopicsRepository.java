5d12f4315012bd52da5523538ab787d63e8e0f9d
Ibrahim Yousre <ib.yousre@gmail.com>
2018-05-13 09:21:44 +0400

    Make repository live

--- app/src/main/java/com/example/ibrahim/ama/data/TopicsRepository.java
+++ app/src/main/java/com/example/ibrahim/ama/data/TopicsRepository.java
@@ -1,14 +1,12 @@
 package com.example.ibrahim.ama.data;
 
+import android.arch.core.util.Function;
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Transformations;
 
 import com.example.ibrahim.ama.data.model.Topic;
-import com.example.ibrahim.ama.util.DatabaseErrorUtils;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,53 +14,34 @@ import java.util.List;
 public class TopicsRepository {
 
     private FirebaseDatabase firebaseDatabase;
+    private Function<DataSnapshot, List<Topic>> topicsDeserializer =
+            new Function<DataSnapshot, List<Topic>>() {
+                @Override
+                public List<Topic> apply(DataSnapshot dataSnapshot) {
+                    List<Topic> topics = new ArrayList<>((int) dataSnapshot.getChildrenCount());
+                    for (DataSnapshot topicSnapShot : dataSnapshot.getChildren()) {
+                        Topic topic = topicSnapShot.getValue(Topic.class);
+                        topic.setUid(topicSnapShot.getKey());
+                        topics.add(topic);
+                    }
+                    return topics;
+                }
+            };
 
     public TopicsRepository(FirebaseDatabase firebaseDatabase) {
         this.firebaseDatabase = firebaseDatabase;
     }
 
     public LiveData<List<Topic>> getAllTopics() {
-        final MutableLiveData<List<Topic>> data = new MutableLiveData<>();
-        firebaseDatabase.getReference("topics").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                List<Topic> topics = new ArrayList<>((int) dataSnapshot.getChildrenCount());
-                for (DataSnapshot topicSnapShot : dataSnapshot.getChildren()) {
-                    Topic topic = topicSnapShot.getValue(Topic.class);
-                    topic.setUid(topicSnapShot.getKey());
-                    topics.add(topic);
-                }
-                data.setValue(topics);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                DatabaseErrorUtils.log(databaseError);
-            }
-        });
-        return data;
+        FirebaseQueryLiveData allTopicsLiveData = new FirebaseQueryLiveData(
+                firebaseDatabase.getReference("topics"));
+        return Transformations.map(allTopicsLiveData, topicsDeserializer);
     }
 
     public LiveData<List<Topic>> getUserTopics(String userUid) {
-        final MutableLiveData<List<Topic>> data = new MutableLiveData<>();
-        firebaseDatabase.getReference("users_topics").child(userUid).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                List<Topic> topics = new ArrayList<>((int) dataSnapshot.getChildrenCount());
-                for (DataSnapshot topicSnapShot : dataSnapshot.getChildren()) {
-                    Topic topic = topicSnapShot.getValue(Topic.class);
-                    topic.setUid(topicSnapShot.getKey());
-                    topics.add(topic);
-                }
-                data.setValue(topics);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                DatabaseErrorUtils.log(databaseError);
-            }
-        });
-        return data;
+        FirebaseQueryLiveData userTopicsLiveData = new FirebaseQueryLiveData(
+                firebaseDatabase.getReference("users_topics").child(userUid));
+        return Transformations.map(userTopicsLiveData, topicsDeserializer);
     }
 
 }
