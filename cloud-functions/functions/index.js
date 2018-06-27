73727cbd9fa462d1ebedca6366ac8f053166f340
Ibrahim Yousre <ib.yousre@gmail.com>
2018-06-27 18:08:02 +0200

    Cloud functions: addUser, newAnswer, updateUserInfo

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -1,18 +1,48 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
-
-// The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
+
 admin.initializeApp();
 
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest((req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    return res.redirect(303, snapshot.ref.toString());
+exports.addUser = functions.auth.user().onCreate(user => {
+  console.log('A new user signed in for the first time.');
+  return admin.database().ref('users').push({
+    name: user.displayName || 'Anonymous',
+    photoUrl: user.photoURL || 'https://ca.slack-edge.com/T95Q9N50F-U98GDP28G-g2e374b00968-48',
+    text: `New User`,
+  }).then(() => {
+    console.log('User added successfully');
+    return;
+  });
+});
+
+exports.newAnswer = functions.database.ref('answers/{answerId}/userId').onCreate((snapshot, context)=>{
+  console.log('new answer by user '+snapshot.val());
+  return admin.database().ref('user_answers').child(snapshot.val()).child(context.params.answerId).set(true);
+});
+
+exports.updateUserInfo = functions.database.ref('users/{userId}').onUpdate((change, context)=>{
+  let userId = context.params.userId;
+  let user = change.after.val();
+  let name = user.name;
+  let photoUrl = user.photoUrl;
+  let title = user.title;
+
+  console.log(`updating user info ${userId}`);
+
+  return admin.database().ref(`user_answers/${userId}`).once('value').then( snap => {
+    let answerKeys = Object.keys(snap.val());
+    let updateObj = {};
+    
+    let n = answerKeys.length;
+    console.log(`updating ${n} answers`);
+
+    answerKeys.forEach(key => {
+      updateObj[`answers/${key}/userId`] = userId;
+      updateObj[`answers/${key}/userName`] = name;
+      updateObj[`answers/${key}/userPhotoUrl`] = photoUrl;
+      updateObj[`answers/${key}/userTitle`] = title;
+    });
+
+    return admin.database().ref().update(updateObj);
   });
 });
