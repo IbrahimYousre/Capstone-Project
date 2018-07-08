6c75f67500eaecb8ee9aea930860fd03ae655d95
Ibrahim Yousre <ib.yousre@gmail.com>
2018-07-08 20:33:10 +0200

    Update cloud functions

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -9,10 +9,42 @@ exports.addUser = functions.auth.user().onCreate(user => {
   return admin.database().ref('users').child(user.uid).set({
     name: user.displayName || 'Anonymous',
     photoUrl: user.photoURL || 'https://ca.slack-edge.com/T95Q9N50F-U98GDP28G-g2e374b00968-48',
-    text: 'New User',
+    title: 'New User',
   });
 });
 
+// add the question awner as a follower
+exports.onQuestionAdded = functions.database.ref('topic_questions/{topicId}/{questionId}').onCreate((snapshot, context)=>{
+  let userId = context.auth.uid;
+  let questionId = context.params.questionId;
+  return admin.database().ref('question_users').child(questionId).child(userId).set(true);
+});
+
+// send notifications to question followers
+exports.sendNotificationAboutAnswer = functions.database.ref('answers/{answerId}').onUpdate((change, context)=>{
+  // only send notification after the server has finished timestamping
+  if(change.after.hasChild('timestamp')){
+    console.log(`Sending notifications`);
+    let answerId = context.params.answerId;
+    let answer = change.after.val();
+    let questionId = answer.questionId;
+    return admin.database().ref(`question_users/${questionId}`).once('value').then( snap => {
+      let userKeys = Object.keys(snap.val());
+      let updateObj = {};
+      let n = userKeys.length;
+      console.log(`Sending notifications to ${n} users`);
+      userKeys.forEach(key => {
+        updateObj[`user_notifications/${key}/${answerId}`] = answer;
+      });
+      return admin.database().ref().update(updateObj);
+    });
+  } else{
+    console.log(`Not sending notifications`);
+    return null;
+  }
+});
+
+// the answer get the time stamp on the server
 exports.addTimestampToAnswer = functions.database.ref('answers/{answerId}').onCreate((snapshot, context)=>{
   let answerId = context.params.answerId;
   return admin.database().ref(`answers/${answerId}/timestamp`).set(admin.database.ServerValue.TIMESTAMP);
@@ -27,17 +59,16 @@ exports.addAnswerToUser = functions.database.ref('answers/{answerId}/userId').on
 // when user update his profile we update all denormalized data in answers
 exports.updateUserInfo = functions.database.ref('users/{userId}').onUpdate((change, context)=>{
   let userId = context.params.userId;
-  let user = change.after.val();
   let old = change.before.val();
+  let user = change.after.val();
   let name = user.name;
   let photoUrl = user.photoUrl;
   let title = user.title;
-  console.log(`Updating profile for user (userId = ${userId})`);
   return admin.database().ref(`user_answers/${userId}`).once('value').then( snap => {
     let answerKeys = Object.keys(snap.val());
     let updateObj = {};
     let n = answerKeys.length;
-    console.log(`updating ${n} answers for user (userId = ${userId})`);
+    console.log(`Updating ${n} answers for user (userId = ${userId})`);
     answerKeys.forEach(key => {
       if(old.name !== name)
         updateObj[`answers/${key}/userName`] = name;
